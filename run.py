bbfe8d88b973cb193ae71cd8ff2165049623dc3b
Sutee Sudprasert <etipitaka@gmail.com>
2013-10-15 14:33:55 +0700

    create data path

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+import os, sys
+import constants
+
+if not os.path.exists(constants.DATA_PATH):
+    os.makedirs(constants.DATA_PATH)
+
 import search.view
 import search.interactor
 import search.presenter
 import search.model
-
-import os, sys
 
 view = search.view.View()
 interactor = search.interactor.Interactor()
