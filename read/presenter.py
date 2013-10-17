9ec6651f1392cfb90f0ba04c574c3a1251ed2f18
Sutee Sudprasert <sutee.s@gmail.com>
2013-10-17 19:03:45 +0700

    fix font size issue on win32

--- read/presenter.py
+++ read/presenter.py
@@ -295,7 +295,11 @@ class Presenter(object):
             self._model.GetTotalPages(self._currentVolume))
         
         content = self._model.GetPage(self._currentVolume, self._currentPage)
+        
+        # work around for fixing font size problem on win32
         self._view.SetText(content)        
+        self._view.SetText(content)
+        
         self._view.StatusBar.SetStatusText(u'', 0)
         self._view.StatusBar.SetStatusText(u'คำค้นหาคือ "%s"'%(self._keywords) if self._keywords is not None and len(self._keywords) > 0 else u'', 1)
         self._view.FormatText(self._model.GetFormatter(self._currentVolume, self._currentPage))
