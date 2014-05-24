2cf61930abdd9a89d5e4942eed9a8eafe889ea14
Sutee Sudprasert <etipitaka@gmail.com>
2014-05-24 22:14:39 +0700

    update building script

--- build_osx.sh
+++ build_osx.sh
@@ -1,4 +1,7 @@
 rm -rf build dist
 rm -f config/*.fav config/history.log
-
-ARCHFLAGS="-arch i386" arch -i386 python setup.py py2app
+export VERSIONER_PYTHON_PREFER_32_BIT=yes
+ARCHFLAGS="-arch i386 -arch x86_64" arch -i386 python setup.py py2app
+ditto --rsrc --arc i386 dist/E-Tipitaka.app dist/E-Tipitaka-32bit.app
+rm -rf dist/E-Tipitaka.app
+mv dist/E-Tipitaka-32bit.app dist/E-Tipitaka.app
