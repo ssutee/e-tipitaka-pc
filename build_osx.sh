c0deb9d2ef52437d660a0ab5aa574afa833a3aa9
Sutee Sudprasert <etipitaka@gmail.com>
2014-10-19 18:14:05 +0700

    OS X 10.10 fixes

--- build_osx.sh
+++ build_osx.sh
@@ -1,7 +1,7 @@
 rm -rf build dist
 rm -f config/*.fav config/history.log
 export VERSIONER_PYTHON_PREFER_32_BIT=yes
-ARCHFLAGS="-arch i386 -arch x86_64" arch -i386 python setup.py py2app
-ditto --rsrc --arc i386 dist/E-Tipitaka.app dist/E-Tipitaka-32bit.app
-rm -rf dist/E-Tipitaka.app
-mv dist/E-Tipitaka-32bit.app dist/E-Tipitaka.app
+ARCHFLAGS="-arch i386 -arch x86_64" arch -i386 python setup.py py2app --arch i386 
+#ditto --rsrc --arc i386 dist/E-Tipitaka.app dist/E-Tipitaka-32bit.app
+#rm -rf dist/E-Tipitaka.app
+#mv dist/E-Tipitaka-32bit.app dist/E-Tipitaka.app
