762a70bbe8734730a12fbb3e79e12a8f11bb2334
Sutee Sudprasert <sutee.s@gmail.com>
2019-05-16 13:45:03 +0700

    update thaipb database

--- build_osx.sh
+++ build_osx.sh
@@ -3,9 +3,9 @@ rm -f config/*.fav config/history.log
 export VERSIONER_PYTHON_PREFER_32_BIT=yes
 ARCHFLAGS="-arch i386 -arch x86_64" arch -i386 python-32 setup.py py2app --arch i386 
 
-ditto --rsrc --arc i386 dist/E-Tipitaka.app dist/E-Tipitaka-32bit.app
-rm -rf dist/E-Tipitaka.app
-mv dist/E-Tipitaka-32bit.app dist/E-Tipitaka.app
+#ditto --rsrc --arc i386 dist/E-Tipitaka.app dist/E-Tipitaka-32bit.app
+#rm -rf dist/E-Tipitaka.app
+#mv dist/E-Tipitaka-32bit.app dist/E-Tipitaka.app
 
 # code signing
 codesign --force --verify --verbose --sign "Developer ID Application: Sutee Sudprasert (A6DJDJ7527)" dist/E-Tipitaka.app/Contents/Frameworks/libwx_macud-2.8.0.8.0.dylib
