cc4da28541ae3748e100309eaad7036e87f41595
Sutee Sudprasert <sutee.s@gmail.com>
2021-03-24 21:17:37 +0700

    update macos scripts

--- build_osx.sh
+++ build_osx.sh
@@ -3,5 +3,3 @@ rm -f config/*.fav config/history.log
 
 pipenv run python setup.py py2app
 
-cp -f /usr/local/opt/xz/lib/liblzma.5.dylib dist/E-Tipitaka.app/Contents/Framworks
-
--- sign_app.sh
+++ sign_app.sh
@@ -1,6 +1,5 @@
 # code signing
-#codesign --force --verify --verbose --sign "Developer ID Application: Sutee Sudprasert (A6DJDJ7527)" dist/E-Tipitaka.app/Contents/Frameworks/*
-#codesign --deep --force --verify --verbose --sign "Developer ID Application: Sutee Sudprasert (A6DJDJ7527)" dist/E-Tipitaka.app/Contents/MacOS/*
+cp -f ./liblzma.5.dylib dist/E-Tipitaka.app/Contents/Frameworks
 codesign --deep --force --verify --no-strict --verbose --sign "Developer ID Application: Sutee Sudprasert (A6DJDJ7527)" dist/E-Tipitaka.app
 
 # verify code signing
