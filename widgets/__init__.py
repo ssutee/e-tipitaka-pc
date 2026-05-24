dd164d36c7dfb616ed6d1f8a621d08f72146cd6e
Sutee Sudprasert <sutee.s@gmail.com>
2026-05-25 06:47:36 +0700

    fix(account): scale account button icon to 32x32
    resources/account.png is 256x256 (copy of edit-notes.png); other
    toolbar bitmaps are 32x32. Without .Scale(32,32) the bitmap button
    rendered ~8x larger than its neighbors. Mirror the same .Scale call
    the notes button uses.

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1985,7 +1985,7 @@ class SearchToolPanel(wx.Panel):
         self._exportButton.SetToolTip(wx.ToolTip(_('Export data')))
 
         self._accountButton = wx.BitmapButton(self, wx.ID_ANY,
-            wx.Bitmap(wx.Image(constants.ACCOUNT_IMAGE, wx.BITMAP_TYPE_PNG)))
+            wx.Bitmap(wx.Image(constants.ACCOUNT_IMAGE, wx.BITMAP_TYPE_PNG).Scale(32,32)))
         self._accountButton.SetToolTip(wx.ToolTip(_('Account / cloud sync')))
 
         self._settingButton = wx.BitmapButton(self, wx.ID_ANY, 
